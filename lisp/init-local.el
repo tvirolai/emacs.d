f37efa4c9d9d3c0e48de12f26a861fb9e19ffcfd
Tuomo Virolainen <tuomo.virolainen@siili.com>
2020-10-08 18:11:38 +0300

    Add config for TypeScript

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -14,6 +14,12 @@
 (use-package flycheck-clj-kondo
   :ensure t)
 
+(use-package tide
+  :ensure t)
+
+(use-package web-mode
+  :ensure t)
+
 (use-package clojure-mode
   :ensure t
   :config
@@ -235,6 +241,42 @@
 (add-hook 'clojure-mode-hook #'aggressive-indent-mode)
 (add-hook 'clojure-mode-hook #'flycheck-mode)
 (add-hook 'clojure-mode-hook #'clojure-mappings)
+
+;; TypeScript settings
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1)
+  (setq typescript-indent-level
+        (or (plist-get (tide-tsfmt-options) ':indentSize) 4))
+  (setq typescript-indent-level 2))
+
+;; TSX
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+
+;; enable typescript-tslint checker
+(flycheck-add-mode 'typescript-tslint 'web-mode)
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 ;; Haskell settings
 
